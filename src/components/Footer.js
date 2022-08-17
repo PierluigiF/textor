cc150378f6c43fa3bba1e7e37e7a53483501c954
Pierluigi Fittipaldi <pierluigifittipaldi@gmail.com>
2022-08-17 15:21:09 +0200

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        <p>
+          Made by{" "}
+          <a href="https://github.com/PierluigiF">Pierluigi Fittipaldi</a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-content">
+        <p>Textor.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
